b125643a607db1c1c215048cddc8c97070f7336b
Eric Friesen <ericfriesen@eric.nerdhome>
2010-07-26 01:05:16 -0700

    testing pages conroller

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -1,59 +1,11 @@
 <?php
-/**
- * Static content controller.
- *
- * This file will render views from views/pages/
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.cake.libs.controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
-/**
- * Static content controller
- *
- * Override this controller by placing a copy in controllers directory of an application
- *
- * @package       cake
- * @subpackage    cake.cake.libs.controller
- * @link http://book.cakephp.org/view/958/The-Pages-Controller
- */
 class PagesController extends AppController {
-
-/**
- * Controller name
- *
- * @var string
- * @access public
- */
 	var $name = 'Pages';
-
-/**
- * Default helper
- *
- * @var array
- * @access public
- */
-	var $helpers = array('Html', 'Session');
-
-/**
- * This controller does not use a model
- *
- * @var array
- * @access public
- */
 	var $uses = array();
+
+	function home() {
+		echo 'home';
+	}
 
 /**
  * Displays a view
@@ -79,7 +31,11 @@ class PagesController extends AppController {
 		if (!empty($path[$count - 1])) {
 			$title_for_layout = Inflector::humanize($path[$count - 1]);
 		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
+		// add this snippet before the last line 
+		if (method_exists($this, $page)) { 
+			$this->$page(); 
+		}
+ 		$this->set(compact('page', 'subpage', 'title_for_layout'));
 		$this->render(implode('/', $path));
 	}
 }
