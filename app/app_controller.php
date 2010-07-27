d87d80ee0a4ddea0429341bd7c0e0efa6251a72f
Eric Friesen <ericfriesen@eric.nerdhome>
2010-07-26 17:30:06 -0700

    login stuff in the header

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,23 +1,25 @@
 <?php
 class AppController extends Controller {
-	
+
 	//add user athentication
-    var $components = array('Auth');
-    
+	var $components = array('Auth');
+
 	function beforeFilter() {
-    
-    	//force athenication against profiles
-    	$this->Auth->fields = array('username' => 'email', 'password' => 'password');
-    	
-    	if(Configure::read('debug') > 0){
-    		//load krumo    		
-    		App::import('Vendor', 'krumo', array('file' => 'krumo/class.krumo.php'));	
-    	}
-	
+		//force athenication against profiles
+		$this->Auth->fields = array('username' => 'email', 'password' => 'password');
+		$user = $this->Session->read('Auth');
+		// Read the user id from the session and if nothing there, set it to 1
+		Configure::write('UID', (!isset($user['User']['id']) ? '1' : $user['User']['id']));
+		Configure::write('LoggedIn', $this->Session->check('Auth.User.username'));
+
+		if(Configure::read('debug') > 0){
+			//load krumo
+			App::import('Vendor', 'krumo', array('file' => 'krumo/class.krumo.php'));
+		}
 	}
-	
+
 	function beforeRender() {
-	
+
 	}
-	
+
 }
--- app/views/elements/pages/navigation.ctp
+++ app/views/elements/pages/navigation.ctp
@@ -1,6 +1,13 @@
 <div id="navigation">
-	<?php echo $html->link('Home', '/'); ?>
-	<?php echo $html->link('Features', '/pages/features'); ?>
-	<?php echo $html->link('Signup', '/#signup'); ?>
-	<?php echo $html->link('Login', '/users/login'); ?>
+<?php
+	echo $html->link('Home', '/');
+	echo $html->link('Features', '/pages/features');
+	// If the user is not logged in, display the signup and login links
+	if (!Configure::read('LoggedIn')) {
+		echo $html->link('Signup', '/#signup');
+		echo $html->link('Login', '/users/login'); 
+	} else {
+		echo 'Welcome Back';
+	}
+?>
 </div>
