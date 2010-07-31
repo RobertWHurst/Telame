5f7b36c4476efbf3adcb8a6ec26e7804b6c731d7
Eric Friesen <efriesen@gmail.com>
2010-07-31 15:23:46 -0700

    image thumbnailing fixes and cache directory automatically created if we have permissions
    Signed-off-by: Eric Friesen <efriesen@gmail.com>

--- app/controllers/components/thumb.php
+++ app/controllers/components/thumb.php
@@ -28,9 +28,9 @@ class ThumbComponent{
 	 * @return bool Success?
 	 * @author Nate Constant
 	 **/
-	function generateThumb($path, $filename, $size) {
+	function generateThumb($baseDir, $dir, $filename, $size) {
 		// Make sure we have the name of the uploaded file and that the Model is specified
-		if(empty($path) || empty($filename)){
+		if(empty($baseDir) || empty($filename)){
 			return false;
 		}
 		if (empty($size)) {
@@ -42,7 +42,8 @@ class ThumbComponent{
 		}
 
 		// verify that the size is greater than 0 ( emtpy file uploaded )
-		if(filesize($path . DS . $filename === 0)){
+		if(filesize($baseDir . $dir . $filename === 0)){
+			Debugger::log('File is empty');
 			return false;
 		}
 
@@ -54,24 +55,29 @@ class ThumbComponent{
 
 		// verify that the filesystem is writable, if not add an error to the object
 		// dont fail if not and let phpThumb try anyway
-		if(!is_writable($path)) {
-			return false;
+		if(!is_writable($baseDir . 'cache')) {
+			if(!mkdir($baseDir . 'cache')) {
+				Debugger::log($baseDir . 'cache' . ' not writable');
+				return false;
+			}
 		}
 
 		// Load phpThumb
 		App::import('Vendor', 'phpThumb', array('file' => 'phpThumb/phpthumb.class.php'));
 		$phpThumb = new phpThumb();
 
-		$phpThumb->setSourceFilename($path . DS . 'profile' . DS . $filename);
+		$phpThumb->setSourceFilename($baseDir . $dir . $filename);
 		$phpThumb->setParameter('w' ,$width);
 		$phpThumb->setParameter('h', $height);
 		$phpThumb->setParameter('zc', 1);
 
 		if($phpThumb->generateThumbnail()){
-			if(!$phpThumb->RenderToFile($path . DS . 'cache' . DS . $filename . '-' . $height . 'x' . $width . '.jpg')) {
+			if(!$phpThumb->RenderToFile($baseDir . 'cache' . DS . $filename . '-' . $height . 'x' . $width . '.jpg')) {
+				Debugger::log('Could not render to file');
 				return false;
 			}
 		} else {
+			Debugger::log('Could not generate thumbnail');
 			return false;
 		}
 		return true;
