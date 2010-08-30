82e2721f860601b87b809e9cba4402aafd1c3653
Robert Hurst <Robert@ThinkTankDesign.ca>
2010-08-30 13:00:02 -0700

    fixed an issue with cakePHP autolink breaking the markdown syntax.
    Signed-off-by: Robert Hurst <Robert@ThinkTankDesign.ca>

--- app/views/helpers/markdown.php
+++ app/views/helpers/markdown.php
@@ -6,7 +6,23 @@ class MarkdownHelper extends AppHelper {
 	var $helpers = array('Text');
 
 	function parse($text) {
-		return $this->output($this->Text->autoLink(Markdown($text)));
+		/**
+		 * The cakePHP autolink method breaks markup syntax for url
+		 * retated elements such as images.
+		 * I believe the best solution is to use the old regular expression
+		 * created to convert links before autolink was implemented.
+		 * It will only convert urls surounded by whitespace to links.
+		 *
+		 * return $this->output($this->Text->autoLink(Markdown($text)));
+		 */
+		 
+		 //format the content with markdown
+		 $text = preg_replace('/((?: http[s]*| [s]*ftp| git):\/\/[^\s]+ )/', '<a href="$1" rel="" target="TELAME_LINK">$1</a>', $text);
+		 
+		 //convert urls to links
+		 $text = Markdown($text);
+		 
+		 return $this->output($text);
 	}
 }
 ?>
