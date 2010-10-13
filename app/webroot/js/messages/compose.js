7d46b283babc33741060920bf10428a2fdb33745
Robert Hurst <Robert@ThinkTankDesign.ca>
2010-10-12 20:33:51 -0700

    just a bit more work and the composer will be done.

--- app/webroot/js/messages/compose.js
+++ app/webroot/js/messages/compose.js
@@ -10,6 +10,9 @@ $(function(){
         root.messageComposerRecipientInputWrap = $('div.recipients', root.messageComposerWrap);
         root.messageComposerRecipientInput = $('#MessageUserSlugs', root.messageComposerRecipientInputWrap);
 
+		//create data objects
+		root.selectedUsers = {};
+		
 		//rebuild ui
 		root.formatUi = function(){
 			
@@ -18,8 +21,14 @@ $(function(){
 					'background': 'transparent',
 					'border': 'none'
 				})
-				.wrap('<div class="fake_input">');
+				.wrap('<div class="fake_input">')
+				.before('<input id="full_name_input" type="text" />'),
 			
+			//bind to the new fake input
+			root.fullNameInput = $('#full_name_input'),
+			
+			//execute the input handler
+			root.inputValueHandler();
 		}
 		
         //post hover handler
@@ -30,36 +39,35 @@ $(function(){
 			root.hintlist = $('#hintList');
 			
 			//set the timer
-			var timer = proccessing = string = i = false;
+			var timer = proccessing = string = i = the_slugs = false;
 			
-			root.messageComposerRecipientInput.keyup(function(event){
-        		
-        		if(proccessing === true){
-        			proccessing = false;
-        			clearTimeout(timer);
-        		}
+			root.fullNameInput.keyup(function(event){
 				
 				//define the function that requests hint data
 				var requestHint = function(){
 					
 					//set the string
 					string = domElement.val().replace(' ', '_');
-									
-					$.get(core.domain + '/m/a/' + string, function(data){
-						
-						//clear the list
-						root.hintlist.empty();
-						
-						if(data.length > 0){
+										
+					//string length must be 2 char or longer to ping the server	
+					if(string.length > 1){
+															
+						$.get(core.domain + '/m/a/' + string, function(data){
 							
-							for(i in data){
+							//clear the list
+							root.hintlist.empty();
+							
+							if(data.length > 0){
 								
-								//create the row
-								addrow(data[i]);
-								
+								for(i in data){
+									
+									//create the row
+									addrow(data[i]);
+									
+								}
 							}
-						}
-					});
+						});
+					}
 				}
 				
 				//define function to create a hint row
@@ -69,22 +77,66 @@ $(function(){
 						return false;
 					}
 					
-					//build the row
-					root.hintlist.append('<div class="row"><div class="user_name">' + userData.full_name + '</div><div class="email">' + userData.email + '</div></div>');
+					if(typeof root.selectedUsers[userData.slug] === 'undefined'){
 					
+						//build the row
+						root.hintlist.append('<div id="' + userData.slug + '" class="row"><div class="user_name">' + userData.full_name + '</div><div class="email">' + userData.email + '</div></div>');
+						
+						//grab the rows
+						$('div.row', '#hintList').click(function(){
+							
+							//save the dom element
+							var domElement = $(this);
+							
+							addUser(domElement.attr('id'));
+							
+						});
+					}
+				}
+				
+				var addUser = function(slug){
+						
+						//add the user slug to the selected array
+						root.selectedUsers[slug] = slug;
+						
+						updateSlugsInput();
+				}
+				
+				var removeUser = function(slug){
+						
+						//add the user slug to the selected array
+						root.selectedUsers.splice(slug);
+						
+						updateSlugsInput();
+					
+				}
+				
+				var updateSlugsInput = function(){
+					//clear the input.val('');
+					
+					//add the slugs to the hidden slug input
+					i = 1;
+					for(k in root.selectedUsers){
+						the_slugs += root.selectedUsers[k];
+						
+						if(i !== root.selectedUsers.length){
+							the_slugs += ', ';
+						}
+						
+						//adv the iderator
+						i += 1;
+					}
+					root.messageComposerRecipientInput.val(the_slugs);
 				}
 				
 				// save the dom element
 				var domElement = $(this);
 				
-				//exit the function if the input is empty
-				if(domElement.val() == ''){
-					return false;	
-				}
+				//clear the list
+				root.hintlist.empty();
 				
-				//set a timeout so requests are only made when the user stops typing
-				timer = setTimeout(requestHint, 500);
-				proccessing = true;
+				//get the hints
+				requestHint();
 				
 			});
 			
@@ -94,8 +146,7 @@ $(function(){
         root.construct = function(){
 
             //bind all of the event handlers
-            root.inputValueHandler();
-            //root.formatUi();
+            root.formatUi();
 
         }
 
