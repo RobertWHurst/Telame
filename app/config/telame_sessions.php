1d16d82b14e811b2dd1cf83fec28c9c4db0c9406
Eric Friesen <efriesen@gmail.com>
2010-10-14 11:03:07 -0700

    we need the domain set to .telame.com for cookies
    Signed-off-by: Eric Friesen <efriesen@gmail.com>

--- app/config/telame_sessions.php
+++ app/config/telame_sessions.php
@@ -7,17 +7,17 @@ $modelName = Configure::read('Session.model');
 $database = Configure::read('Session.database');
 
 if (empty($database)) {
-    $database = 'default';
+	$database = 'default';
 }
 
 $settings = array(
-    'class' => 'Session',
-    'alias' => 'Session',
-    'table' => 'cake_sessions',
-    'ds' => $database
+	'class' => 'Session',
+	'alias' => 'Session',
+	'table' => 'cake_sessions',
+	'ds' => $database
 );
 if (!empty($modelName)) {
-    $settings['class'] = $modelName;
+	$settings['class'] = $modelName;
 }
 /**
  * Load the Session Model.
@@ -28,19 +28,20 @@ ClassRegistry::init($settings);
  * Setup any custom ini settings needed.
  */
 if (empty($_SESSION)) {
-    if ($iniSet) {
-        ini_set('session.use_trans_sid', 0);
-        ini_set('url_rewriter.tags', '');
-        ini_set('session.save_handler', 'user');
-        ini_set('session.serialize_handler', 'php');
-        ini_set('session.use_cookies', 1);
-        ini_set('session.name', Configure::read('Session.cookie'));
-        ini_set('session.cookie_lifetime', Configure::read('Session.timeout') * 60);
+	if ($iniSet) {
+		ini_set('session.use_trans_sid', 0);
+		ini_set('url_rewriter.tags', '');
+		ini_set('session.save_handler', 'user');
+		ini_set('session.serialize_handler', 'php');
+		ini_set('session.use_cookies', 1);
+		ini_set('session.name', Configure::read('Session.cookie'));
+		ini_set('session.cookie_lifetime', Configure::read('Session.timeout') * 60);
 		$this->path = '/';
-        ini_set('session.cookie_path', $this->path);
-        ini_set('session.auto_start', Configure::read('Session.start'));
-        ini_set('session.referer_check', null);
-    }
+		ini_set('session.cookie_path', $this->path);
+		ini_set('session.cookie_domain', env('HTTP_BASE'));
+		ini_set('session.auto_start', Configure::read('Session.start'));
+		ini_set('session.referer_check', null);
+	}
 }
 
 /**
@@ -48,12 +49,12 @@ if (empty($_SESSION)) {
  * This is a straight copy from cake_session.php.
  */
 session_set_save_handler(
-    array('CakeSession', '__open'),
-    array('CakeSession', '__close'),
-    array('CakeSession', '__read'),
-    array('CakeSession', '__write'),
-    array('CakeSession', '__destroy'),
-    array('CakeSession', '__gc')
+	array('CakeSession', '__open'),
+	array('CakeSession', '__close'),
+	array('CakeSession', '__read'),
+	array('CakeSession', '__write'),
+	array('CakeSession', '__destroy'),
+	array('CakeSession', '__gc')
 );
 
 /**
